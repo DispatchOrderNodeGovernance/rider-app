bc46669c6289892e9dc977327588c992723e3fef
willwilson <qtvhao@gmail.com>
2024-05-23 00:57:02 +0700

    chore: Add timeout and abort controller to onRequestPost function

--- functions/locations.js
+++ functions/locations.js
@@ -3,6 +3,8 @@ export async function onRequestPost(context) {
     let { driver_id, status, in_trip, latitude, longitude } = body;
     let forwardTo = "https://0xyshn92yf.execute-api.ap-southeast-1.amazonaws.com/production_stage/locations"
     try{
+        const controller = new AbortController()
+        const timeoutId = setTimeout(() => controller.abort(), 20)
         let response = await fetch(forwardTo, {
             method: 'POST',
             headers: {
@@ -10,7 +12,9 @@ export async function onRequestPost(context) {
             },
             body: JSON.stringify({ driver_id, status, in_trip, latitude, longitude }),
             timeout: 20,
+            signal: controller.signal
         });
+        clearTimeout(timeoutId)
         return new Response(await response.text(), {status: response.status});
     }catch(e){
         return new Response({error: e.message}, {status: 500});
