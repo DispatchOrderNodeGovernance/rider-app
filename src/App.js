74f6d30e710124100152d83fc1a7fc54cfbb95e2
willwilson <qtvhao@gmail.com>
2024-05-22 17:51:26 +0700

    chore: Refactor LocationTracker component to improve geolocation tracking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
 import LocationTracker from './components/LocationTracker';
 function App() {
-  const [data, setData] = useState({
-    "currentLocation": {
-      "latitude": 0,
-      "longitude": 0
-    },
-  });
-  useEffect(() => {
-    let payload = {
-      "currentLocation": {
-        "latitude": 10.776889,
-        "longitude": 106.700806
-      },
-    };
-    setData(payload);
-  }, []);
   return (
     <div className="App">
       <header className="App-header">
