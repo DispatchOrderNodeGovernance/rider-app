4ca0a18cad192565e4913015892eca610b9c462c
willwilson <qtvhao@gmail.com>
2024-05-23 00:46:18 +0700

    chore: Forward location data to server in onRequestPost function

--- functions/locations.js
+++ functions/locations.js
@@ -1,4 +1,18 @@
 export async function onRequestPost(context) {
     let body = await context.request.json();
-    return new Response(`JSON sent successfully: ${JSON.stringify(body)}`);
+    let { driver_id, status, in_trip, latitude, longitude } = body;
+    let forwardTo = "https://0xyshn92yf.execute-api.ap-southeast-1.amazonaws.com/production_stage/locations"
+    try{
+        let response = await fetch(forwardTo, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ driver_id, status, in_trip, latitude, longitude }),
+            timeout: 20,
+        });
+        return new Response(await response.json());
+    }catch(e){
+        return new Response({error: e.message}, {status: 500});
+    }
 }
