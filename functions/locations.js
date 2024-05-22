0977873cbcd42c70bb2c6b2989a3cf3dee4cb2c6
willwilson <qtvhao@gmail.com>
2024-05-23 01:08:43 +0700

    chore: Refactor onRequestPost function to simplify code and remove unnecessary timeout and abort controller

--- functions/locations.js
+++ functions/locations.js
@@ -2,21 +2,21 @@ export async function onRequestPost(context) {
     let body = await context.request.json();
     let { driver_id, status, in_trip, latitude, longitude } = body;
     let forwardTo = "https://0xyshn92yf.execute-api.ap-southeast-1.amazonaws.com/production_stage/locations"
-    try{
-        const controller = new AbortController()
-        const timeoutId = setTimeout(() => controller.abort(), 20)
-        let response = await fetch(forwardTo, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ driver_id, status, in_trip, latitude, longitude }),
-            timeout: 20,
-            signal: controller.signal
-        });
-        clearTimeout(timeoutId)
-        return new Response(await response.text(), {status: response.status});
-    }catch(e){
-        return new Response({error: e.message}, {status: 500});
-    }
+    let controller = new AbortController();
+    setTimeout(() => controller.abort(), 6);
+    fetch(forwardTo, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ driver_id, status, in_trip, latitude, longitude }),
+        signal: controller.signal
+    });
+    return new Response(JSON.stringify({
+        driver_id,
+        status,
+        in_trip,
+        latitude,
+        longitude
+    }), {status: 200});
 }
