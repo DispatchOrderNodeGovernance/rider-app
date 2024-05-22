ce9cc14ebe50422146f5c06265bf230fca8fb33d
willwilson <qtvhao@gmail.com>
2024-05-23 00:38:58 +0700

    feat: Update onRequestPost function to use async/await for JSON parsing

--- functions/locations.js
+++ functions/locations.js
@@ -1,4 +1,4 @@
-export function onRequestPost(context) {
-    let body = context.request.json();
+export async function onRequestPost(context) {
+    let body = await context.request.json();
     return new Response(`JSON sent successfully: ${JSON.stringify(body)}`);
 }
