7e7571820b04cd8967ec88d34955081ab59124e8
willwilson <qtvhao@gmail.com>
2024-05-23 11:29:20 +0700

    chore: Refactor onRequestPost function to include pickup and dropoff locations in request body

--- functions/request-ride.js
+++ functions/request-ride.js
@@ -5,23 +5,21 @@ export async function onRequestPost(context) {
         dropoff_location: dropoffLocation,
         ride_type: rideType
       } = body;
-    /* let forwardTo = "https://0xyshn92yf.execute-api.ap-southeast-1.amazonaws.com/production_stage/locations"
+    let forwardTo = "https://0xyshn92yf.execute-api.ap-southeast-1.amazonaws.com/production_stage/dispatch"
     let controller = new AbortController();
     setTimeout(() => controller.abort(), 6);
-    fetch(forwardTo, {
+    let dispatchResponse = await fetch(forwardTo, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ driver_id, status, in_trip, latitude, longitude }),
+        body: JSON.stringify({
+            pickup_location: pickupLocation,
+            dropoff_location: dropoffLocation,
+            ride_type: rideType
+        }),
         signal: controller.signal
-    }); */
-    return new Response(JSON.stringify({
-        ride_id: 'ride_123',
-        status: 'in_progress',
-        pickup_location: pickupLocation,
-        dropoff_location: dropoffLocation,
-        estimated_arrival_time: '2021-09-01T12:00:00Z',
-        ride_type: rideType
-    }), {status: 200});
+    });
+
+    return new Response(JSON.stringify(await dispatchResponse.json()), { status: 200, headers: { 'Content-Type': 'application/json' } });
 }
